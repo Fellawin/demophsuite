ed5c16e45cc18b1b3b4885056b1f34633c7a9a3b
Carl Jason T. Costiniano <ccostiniano@i4asiacorp.com>
2024-03-20 14:26:24 +0800

    Save

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './public/output.css'
+import './dist/assets/index-CGBjB4Sv.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
