34c5f43a0d087e5d61012bdb5ce194ba08a8c02a
Carl Jason T. Costiniano <ccostiniano@i4asiacorp.com>
2024-04-08 14:05:06 +0800

    Update CSS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './dist/assets/index-CJL-vP6m.css'
+// import './dist/assets/index-CJL-vP6m.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
